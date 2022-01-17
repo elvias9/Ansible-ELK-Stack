61c1c429802bef7e078f208a20f2c35082d44513
ash-ley <ashleyritchie1871@hotmail.co.uk>
2022-01-17 09:26:25 +0000

    Update fetch-ips script to ignore null response
    Also added demo servers

--- fetch-ips.sh
+++ fetch-ips.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-aws ec2 describe-instances --filters "Name=tag:Name, Values=Kibana" | jq -r '.Reservations[].Instances[].PublicIpAddress' > kibana-ip.txt
-aws ec2 describe-instances --filters "Name=tag:Name, Values=elasticsearch-server" | jq -r '.Reservations[].Instances[].PrivateIpAddress' > elasticsearch-ip.txt
-aws ec2 describe-instances --filters "Name=tag:Name, Values=Logstash" | jq -r '.Reservations[].Instances[].PrivateIpAddress' > logstash-ip.txt
+aws ec2 describe-instances --filters "Name=tag:Name, Values=Kibana" | jq -r '.Reservations[].Instances[].PublicIpAddress | select( . != null )' > kibana-ip.txt
+aws ec2 describe-instances --filters "Name=tag:Name, Values=elasticsearch-server" | jq -r '.Reservations[].Instances[].PrivateIpAddress | select( . != null )' > elasticsearch-ip.txt
+aws ec2 describe-instances --filters "Name=tag:Name, Values=Logstash" | jq -r '.Reservations[].Instances[].PrivateIpAddress | select( . != null )' > logstash-ip.txt
+aws ec2 describe-instances --filters "Name=tag:Name, Values=Test Server Syslog" | jq -r '.Reservations[].Instances[].PrivateIpAddress | select( . != null )' > apache-ip.txt
+aws ec2 describe-instances --filters "Name=tag:Name, Values=Test Server Apache" | jq -r '.Reservations[].Instances[].PrivateIpAddress | select( . != null )' > syslog-ip.txt
