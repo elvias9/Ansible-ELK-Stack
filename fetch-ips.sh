e263a9f2003b8a33e4a5efdfa0be7709e060a9da
elvia-si <elvia.sicuro@cloudreach.com>
2022-01-18 18:27:13 +0100

    fixed connection to elasticsearch

--- fetch-ips.sh
+++ fetch-ips.sh
@@ -6,4 +6,4 @@ aws ec2 describe-instances --filters "Name=tag:Name, Values=Test Server Syslog" 
 aws ec2 describe-instances --filters "Name=tag:Name, Values=Test Server Apache" | jq -r '.Reservations[].Instances[].PrivateIpAddress | select( . != null )' > apache-ip.txt
 
 BASTION=`aws ec2 describe-instances --filters "Name=tag:Name, Values=Kibana" | jq -r '.Reservations[].Instances[].PublicIpAddress | select( . != null )'`
-echo -en "Host $BASTION\n\tHostname $BASTION\n\tPort 22\n\tUser ubuntu\n\tIdentityFile ~/.ssh/Team1KeyPair.pem\n\nHost 10.10.*.*\n\tProxyJump $BASTION\n\tIdentityFile ~/.ssh/Team1KeyPair.pem" > ssh.config
+echo "Host $BASTION\n\tHostname $BASTION\n\tPort 22\n\tUser ubuntu\n\tIdentityFile ~/.ssh/Team1KeyPair.pem\n\nHost 10.10.*.*\n\tProxyJump $BASTION\n\tIdentityFile ~/.ssh/Team1KeyPair.pem" > ssh.config
